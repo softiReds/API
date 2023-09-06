2322a16d8cb0dcb83f0cd5686b1f096f610d8091
Santiago Rojas <santirojastr12@gmail.com>
2023-09-06 07:32:20 -0500

    Implementacion de metodos para operaciones en la base de datos del servicio 'UsuarioService'

--- Services/UsuarioService.cs
+++ Services/UsuarioService.cs
@@ -1,16 +1,59 @@
+using APIGestiónUsuarios.Context;
 using APIGestiónUsuarios.Models;
 
 namespace APIGestiónUsuarios.Services;
 
 public class UsuarioService : IUsuarioService
 {
+    private readonly GestionUsuariosContext _dbContext;
 
+    public UsuarioService(GestionUsuariosContext dbContext) => _dbContext = dbContext;
+
+    public async Task Post(Usuario usuario)
+    {
+        await _dbContext.Usuarios.AddAsync(usuario);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public IEnumerable<Usuario> Get()
+    {
+        return _dbContext.Usuarios;
+    }
+
+    public async Task Put(Usuario usuario, Guid id)
+    {
+        Usuario usuarioActualizar = _dbContext.Usuarios.Find(id);
+
+        if (usuarioActualizar != null)
+        {
+            usuarioActualizar.UsuarioNombre = usuario.UsuarioNombre;
+            usuarioActualizar.UsuarioApellido = usuario.UsuarioApellido;
+            usuarioActualizar.UsuarioCorreoElectronico = usuario.UsuarioCorreoElectronico;
+            usuarioActualizar.UsuarioContraseña = usuario.UsuarioContraseña;
+            usuarioActualizar.UsuarioFechaCreacion = usuario.UsuarioFechaCreacion;
+            usuarioActualizar.UsuarioFechaActualizacion = usuario.UsuarioFechaActualizacion;
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+
+    public async Task Delete(Guid id)
+    {
+        Usuario usuarioEliminar = _dbContext.Usuarios.Find(id);
+
+        if (usuarioEliminar != null)
+        {
+            _dbContext.Remove(usuarioEliminar);
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
 }
 
 public interface IUsuarioService
 {
     IEnumerable<Usuario> Get();
-    Task Put(Usuario usuario, Guid id);
     Task Post(Usuario usuario);
+    Task Put(Usuario usuario, Guid id);
     Task Delete(Guid id);
 }
